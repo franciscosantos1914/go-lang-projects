05bff57a48a74d25579c55794c81840bff34e5c3
francisco.santos <>
2024-01-16 01:46:33 +0100

    chore(file): create variables example

---
+++ variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Initial"
+	fmt.Println(a)
+
+	var b, c int = 2, 3
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+
+	var g string
+	fmt.Println(g)
+}
